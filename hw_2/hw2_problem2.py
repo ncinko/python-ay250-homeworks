b1827858024928677304ecf931e8474a62dda358
ncinko <ncinko@berkeley.edu>
2018-02-12 00:55:57 -0800

    .

---
+++ hw_2/hw2_problem2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+Nick Cinko
+AY250 HW2 Problem 2
+Reproduce in matplotlib the provided plot stocks.png
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = np.load("data/ny_temps")
+print(data)
