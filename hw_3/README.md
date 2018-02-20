ce3ca3d4d051a86b6e103422852caed962490233
ncinko <ncinko@berkeley.edu>
2018-02-20 02:25:12 -0800

    final hw3 problem 2 solution; harmonics are tricky

--- hw_3/README.md
+++ hw_3/README.md
@@ -6,3 +6,65 @@ instructions for my_credentials.py (how to use API services, e-mail, etc.)
 
 Problem 2:
 
+Code solution: hw3_problem2.py  As written, a 'sound_files' directory is assumed to be in the same directory as the script; it runs over all .aif files in 'sound_files'.
+power_spectrum.png contains an example frequency power spectrum obtained from a FFT.  We see the fundamental frequency at ~175 Hz (F3) and the next overtone at ~350 Hz.
+It seems to get the notes pretty well; 5.aif is a bit weird to me because the G2 is really dominant by ear, but the power spectrum shows a ~147 Hz (D3) signal.  Also, I hear
+an octave in 2.aif, so I added the amplitude comparison criterion--but this showed the higher power F5 frequency in F4_CathedralOrgan.aif.  I'm not sure how to handle both
+these cases simultaneously given the results of the FFT.
+
+Text output from program (I re-ordered the output):
+
+Notes found in 1.aif:
+C4
+D4
+G4
+=====================
+Notes found in 2.aif:
+F3
+F4
+=====================
+Notes found in 3.aif:
+A4
+=====================
+Notes found in 4.aif:
+C4
+=====================
+Notes found in 5.aif:
+G2
+D3
+=====================
+Notes found in 6.aif:
+C5
+=====================
+Notes found in 7.aif:
+D5
+=====================
+Notes found in 8.aif:
+F4
+=====================
+Notes found in 9.aif:
+G3
+=====================
+Notes found in 10.aif:
+C2
+=====================
+Notes found in 11.aif:
+E2
+=====================
+Notes found in 12.aif:
+C2
+=====================
+Notes found in A4_PopOrgan.aif:
+A4
+=====================
+Notes found in C4+A4_PopOrgan.aif:
+C4
+A4
+=====================
+Notes found in F3_PopOrgan.aif:
+F3
+=====================
+Notes found in F4_CathedralOrgan.aif:
+F4
+F5
+=====================
--- hw_3/hw3_problem2.py
+++ hw_3/hw3_problem2.py
@@ -5,7 +5,7 @@ Nick Cinko
 AY250 HW3 Problem 2
 Goal: Write a program that identifies musical notes from sound (AIFF) files.
 """
-
+from os import listdir
 import aifc  #used to open audio files
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft
@@ -14,7 +14,7 @@ import numpy as np
 
 #find frequency of "nth" note, where the 45th note is defined to be 440 Hz (A4); conspiracy theorists prefer 432 Hz
 def note_to_freq(n):
-    return np.power(2, (n - 45.)/12.)*440
+    return np.power(2, (n - 33.)/12.)*440
 
 #standard note names of modern (ascending) chromatic scale
 notes = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"]
@@ -23,81 +23,99 @@ notes = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"]
 notedict={}
 
 #loop to fill note dictionary and number relative octaves
-for j in range(5):
+for j in range(4):
     for i in range(12):
-        notedict[notes[i]+str(j+1)] = note_to_freq(j*12+i)
+        notedict[notes[i]+str(j+2)] = note_to_freq(j*12+i)
 
 #loop to remove sharp/flat notes since it appears we have none; not necessary, but it will make the search faster
 for key in list(notedict): #dictionary will change size in loop, so get keys list at start of loop
     if "#" in key:
         notedict.pop(key)  
 
-print(notedict)
 
-#open audio files
-file = aifc.open('sound_files/F4_CathedralOrgan.aif', 'rb')
+def AnalyzeFile(filename):
+    
+    #open audio files
+    file = aifc.open(directory + '/' + filename, 'rb')
+    
+    #get number of audio frames in file & sample rate; playtime = (# frames)/(sample rate)
+    nframes = file.getnframes()
+    samplerate = 2*file.getframerate()  #not sure why I need to double this, but my frequencies were all 50% their normal value
+    
+    #read all audio frames
+    rawdata = file.readframes(nframes)
+    
+    #create numpy array from audio data; logic borrowed from https://gist.github.com/arunaugustine/5551446
+    y = np.fromstring(rawdata, np.short).byteswap()
+    audiolist = []
+    audiolist.append(y)
+    x = np.array(audiolist)
+    
+    #fast Fourier transform the audio data & generate corresponding frequency values
+    X = fft(x[0])
+    freqs = fftfreq(len(x[0]))*samplerate
+    freq_spacing = samplerate/len(x[0])
+    
+    #frequencies/amplitudes are symmetric about f=0 Hz; split the array and take positive frequencies
+    sliced = np.split(X,2)
+    sliced_freqs = np.split(freqs,2)
+    search_amps = np.abs(sliced[0])/len(x[0])  #fft returns complex values that must be scaled by the length of initial array
+    search_freqs = sliced_freqs[0]
+    
+    #uncomment for plots
+    """#create figure/axis from matplotlib
+    fig, ax = plt.subplots()
+    fig.suptitle(filename + ' Power Spectrum', fontsize=14, fontweight='bold')
+    #plot fft amplitudes against frequencies
+    ax.plot(search_freqs, search_amps)
+    ax.set_xlabel('frequency (Hz)')
+    ax.set_ylabel('power')
+    ax.set_xlim(0,2400)
+    ax.set_ylim(0, np.amax(search_amps))"""
+    
+    possiblenotes = []  #this will store the note names & amplitudes of peaks found in the power spectrum plots
+    windowsize = 100  #roughly 6 Hz wide with given frequency spacing
+    #the 'instruments' appear to produce harmonics of the form f, 2f, 3f, etc. where f is the fundamental frequency
+    #we will search for peaks in a frequency window around each fundamental note frequency
+    for note in notedict:
+        frequency = notedict[note]
+        freq_bin = int(frequency/freq_spacing)  #convert desired frequency to an index (bin) of our numpy array
+        avg_amp = np.amax(search_amps[int(freq_bin - windowsize/2):int(freq_bin + windowsize/2)])  #get max amplitude in the frequency window
+        #print(note, avg_amp)
+        if avg_amp > 50:
+            possiblenotes.append([note,avg_amp])  #append note name & amplitude if there's a peak
+    
+    #Attempt to eliminate redundant overtones below
+    newnote_truth = [1]*len(possiblenotes)  #Boolean list (True will mean a possible note is unique and not a harmonic)
+    threshold = .08  #somewhat arbitrary threshold for harmonic search
+    harmonics = []
+    for j in range(20):
+        harmonics.append(2 + j)
+    
+    
+    for i in range(len(possiblenotes)):  #loop through all possible notes
+        note1 = notedict[possiblenotes[i][0]]
+        for j in range(i+1, len(possiblenotes)):  #loop through possible notes higher (in frequency) than outer loop note
+            note2 = notedict[possiblenotes[j][0]]
+            for ratio in harmonics:  #check if note 2 is a multiple of note 1
+                if np.abs(note2/note1 - ratio) < threshold and possiblenotes[i][1] > possiblenotes[j][1] :  #amplitude comparison as well to attempt to allow octaves
+                    newnote_truth[j] = 0  #note 2 is probably an overtone
+                    
+    #print(possiblenotes)
+    #print out results
+    print('Notes found in '+ filename + ':')
+    for i in range(len(newnote_truth)):
+        if newnote_truth[i] == True:
+            print(possiblenotes[i][0])
+    print('=====================')       
 
-#get number of audio frames in file & sample rate; playtime = (# frames)/(sample rate)
-nframes = file.getnframes()
-samplerate = file.getframerate()
+    
+    
+directory = 'sound_files'
 
-#read all audio frames
-rawdata = file.readframes(nframes)
-
-#create numpy array from audio data; logic borrowed from https://gist.github.com/arunaugustine/5551446
-y = np.fromstring(rawdata, np.short).byteswap()
-audiolist = []
-audiolist.append(y)
-x = np.array(audiolist)
-
-#fast Fourier transform the audio data & generate corresponding frequency values
-X = fft(x[0])
-freqs = fftfreq(len(x[0]))*samplerate
-freq_spacing = samplerate/len(x[0])
-
-#frequencies/amplitudes are symmetric about f=0 Hz; split the array and take positive frequencies
-sliced = np.split(X,2)
-sliced_freqs = np.split(freqs,2)
-search_amps = np.abs(sliced[0])/len(x[0])  #fft returns complex values that must be scaled by the length of initial array
-search_freqs = sliced_freqs[0]
-
-#create figure/axis from matplotlib
-fig, ax = plt.subplots()
-
-#plot fft amplitudes against frequencies
-ax.plot(search_freqs, search_amps)
-ax.set_xlim(100,800)
-ax.set_ylim(0, 50)
-
-possiblenotes = []
-windowsize = 100
-
-#the 'instruments' appear to produce harmonics of the form f/2, f, 3f/2, 2f, etc. where f is the fundamental frequency
-for note in notedict:
-    frequency = notedict[note]/2
-    freq_bin = int(frequency/freq_spacing)
-    avg_amp = np.average(search_amps[int(freq_bin - windowsize/2):int(freq_bin + windowsize/2)])
-    if avg_amp > 20:
-        possiblenotes.append(note)
-
-newnote_truth = [1]*len(possiblenotes)
-threshold = .05
-harmonics = []
-for j in range(20):
-    harmonics.append(1.5 + j*0.5)
-
-
-for i in range(len(possiblenotes)):
-    note1 = notedict[possiblenotes[i]]    
-    for j in range(i+1, len(possiblenotes)):
-        note2 = notedict[possiblenotes[j]]
-        for ratio in harmonics:
-            if np.abs(note2/note1 - ratio) < threshold:
-                print(note1, note2, i, j)
-                newnote_truth[j] = 0
-                
-print(newnote_truth)
-print(possiblenotes)
+for file in listdir(directory):
+    if 'aif' in file:
+        AnalyzeFile(file)
                
 
     #print(search_freqs[int(frequency/freq_spacing)], frequency)
