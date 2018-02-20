f7a6b36d72c1f08625fcee1d5bf6b2dc6f1517bb
ncinko <ncinko@berkeley.edu>
2018-02-19 16:54:46 -0800

    unpack sound files, move to new hw3 repo

---
+++ hw_3/README.md
@@ -0,0 +1,8 @@
+﻿hw_3 README
+
+Problem 1:
+
+instructions for my_credentials.py (how to use API services, e-mail, etc.)
+
+Problem 2:
+
---
+++ hw_3/hw3_problem2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+Nick Cinko
+AY250 HW3 Problem 2
+Goal: Reproduce in matplotlib the provided plot stocks.png
+"""
